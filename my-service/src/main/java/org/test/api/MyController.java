b158ba2d2b940ce69146ae31eb9dad7ac2dd8545
KenavR <kenavr@gmail.com>
2016-05-14 18:25:11 +0200

    Fixed issue by updating to BRIXTON version and changed code where needed.

--- my-service/src/main/java/org/test/api/MyController.java
+++ my-service/src/main/java/org/test/api/MyController.java
@@ -1,10 +1,7 @@
 package org.test.api;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,8 +11,10 @@ import java.util.Map;
 public class MyController {
 
 
-    @RequestMapping(method = RequestMethod.POST)
-    public @ResponseBody Map<String, String> getTest(@RequestParam("id") Long id) {
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json; charset=utf-8")
+    public @ResponseBody Map<String, String> getTest(@RequestBody Map<String, Long> params) {
+
+        System.out.println("GET TEST: " + params.get("id"));
 
         Map<String, String> response = new HashMap<>();
 
--- public-api/src/main/java/org/test/api/services/MyServiceService.java
+++ public-api/src/main/java/org/test/api/services/MyServiceService.java
@@ -1,7 +1,11 @@
 package org.test.api.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,16 +21,21 @@ public class MyServiceService {
     private final String serviceUrl;
 
     public MyServiceService() {
-        this.serviceUrl = "http://MY-SERVICE";
+        this.serviceUrl = "http://my-service";
     }
 
     public Map<String, String> getTest() {
 
-        Map<String, String> vars = new HashMap<>();
-        vars.put("id", "1");
-
         restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
 
-        return restTemplate.postForObject(serviceUrl+"/test", "", Map.class, vars);
+        // Add converters
+        // Note I use the Jackson Converter, I removed the http form converter
+        // because it is not needed when posting String, used for multipart forms.
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+        Map<String, Long> vars = new HashMap<>();
+        vars.put("id", 1L);
+
+        return restTemplate.postForObject(serviceUrl+"/test", vars, Map.class);
     }
 }
